71c342207570c6bdb84972d0b513e41cd8c0ab26
Felipe Macedo <147464434+Lefpe@users.noreply.github.com>
2024-04-10 19:53:05 -0300

    Update (atv1)questao4.hs

--- (atv1)questao4.hs
+++ (atv1)questao4.hs
@@ -1,1 +1,19 @@
+--verifica se é um número perfeito--
+ehPerfeito :: Int -> Bool
+ehPerfeito n = n == sumDivisores n 1
+
+sumDivisores :: Int -> Int -> Int
+sumDivisores n divisor
+    | divisor == n = 0
+    | n `mod` divisor == 0 = divisor + sumDivisores n (divisor + 1)
+    | otherwise = sumDivisores n (divisor + 1)
+
+main :: IO ()
+main = do
+    putStrLn "Digite um número natural:"
+    input <- getLine
+    let n = read input :: Int
+    if ehPerfeito n
+        then putStrLn $ show n ++ " é um número perfeito."
+        else putStrLn $ show n ++ " não é um número perfeito."
 
