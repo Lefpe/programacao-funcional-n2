7b269af8b97cace55965cc9f82c7a5c1c7cd4bf7
Felipe Macedo <147464434+Lefpe@users.noreply.github.com>
2024-04-10 20:06:44 -0300

    Update (atv1)questao2.hs

--- (atv1)questao2.hs
+++ (atv1)questao2.hs
@@ -1,1 +1,21 @@
+-- Verifica se um número é primo
+ehPrimo :: Int -> Bool
+ehPrimo n
+  | n <= 1 = False
+  | otherwise = null [x | x <- [2..floor (sqrt (fromIntegral n))], n `mod` x == 0]
 
+-- Encontrar n primo
+nPrimo :: Int -> Int
+nPrimo n = enesimoPrimo n 2
+  where
+    enesimoPrimo 1 primoAtual = primoAtual
+    enesimoPrimo k primoAtual
+      | ehPrimo (primoAtual + 1) = enesimoPrimo (k - 1) (primoAtual + 1)
+      | otherwise = enesimoPrimo k (primoAtual + 1)
+
+main :: IO ()
+main = do
+  putStrLn "Digite o valor de n:"
+  nStr <- getLine
+  let n = read nStr :: Int
+  putStrLn $ "O " ++ show n ++ "º primo é: " ++ show (nPrimo n)
